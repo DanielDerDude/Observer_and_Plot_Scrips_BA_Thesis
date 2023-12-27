c106b623d278e3bfc4cbd99d5004f64addfbc8ca
DanielDerDude <xDanielFronk@gmx.de>
2023-12-27 13:06:52 +0100

    wirst display of offsets

--- python_utils/serial_plot_csi_live.py
+++ python_utils/serial_plot_csi_live.py
@@ -1,6 +1,5 @@
 import sys
 import matplotlib.pyplot as plt
-#import math
 import numpy as np
 import collections
 from wait_timer import WaitTimer
@@ -12,12 +11,12 @@ print_stats_wait_timer = WaitTimer(1.0)
 render_plot_wait_timer = WaitTimer(0.2)
 
 # Deque definition
-temp_edges = collections.deque(maxlen=3)
-perm_amp = collections.deque(maxlen=100)
-perm_phase = collections.deque(maxlen=100)
+offsets = collections.deque(maxlen=100)
 
-# Variables to store CSI statistics
-offset_count = 0
+# some stuff here
+offset_index = 0
+last_gpio = 0
+last_timestamp = 0
 
 # Create figure for plotting
 plt.ion()
@@ -27,15 +26,14 @@ fig.canvas.draw()
 plt.show(block=False)
 
 
-def carrier_plot(amp):
+def carrier_plot(os):
     plt.clf()
-    df = np.asarray(amp, dtype=np.int32)
-    # Can be changed to df[x] to plot sub-carrier x only (set color='r' also)
-    plt.plot(range(100 - len(amp), 100), df[:, subcarrier], color='r')
-    plt.xlabel("Time")
-    plt.ylabel("Amplitude")
-    plt.xlim(0, 100)
-    plt.title(f"Amplitude plot of Subcarrier {subcarrier}")
+    y = np.asarray(os, dtype=np.int64)
+    plt.plot(range(1, len(y), 1), y, color='r')
+    plt.xlabel("offset index")
+    plt.ylabel("delta t")
+    #plt.xlim(0, 100)
+    plt.title("Test yoyoyoooo")
     # to flush the GUI events
     fig.canvas.flush_events()
     plt.show()
@@ -44,19 +42,24 @@ def carrier_plot(amp):
 def process(res):
     # Parser
     parts = res.split()
-    timestamp = int(parts[-1])
-    temp_edges.append(timestamp)
-    diff1 = temp_edges[0] - temp_edges[1]
-    diff2 = temp_edges[0] - temp_edges[1]
+    gpio = parts[0]
+
+    if (gpio != last_gpio and last_gpio != 0):      #only compute offsets if gpios are not identical
+        timestamp = int(parts[-1])
+        offsets.append(timestamp-last_timestamp)
+        offset_index + 1
+
+    last_gpio = gpio
+
 
 while True:
     line = readline()
-    if "EDGE RISING" in line:
+    if "EDGE RISING" in line:           # only intrested in rising edges
         process(line)
 
         #if print_stats_wait_timer.check():
         #    print_stats_wait_timer.update()
 
-        if render_plot_wait_timer.check() and len(perm_amp) > 2:
-            render_plot_wait_timer.update()
-            carrier_plot(perm_amp)
+        #if render_plot_wait_timer.check() and len(offsets) > 2:
+        #    render_plot_wait_timer.update()
+        #    carrier_plot(offsets)
